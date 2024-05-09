6ec63535761b644995ecce31e210bef4ee330111
Luan Luiz Laurentino <luan.llaurentino@gmail.com>
2024-05-09 16:44:28 -0300

    Desafio Concluido!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -7,7 +7,6 @@ namespace DesafioProjetoHospedagem.Models
         public int DiasReservados { get; set; }
 
         public Reserva() { }
-
         public Reserva(int diasReservados)
         {
             DiasReservados = diasReservados;
@@ -17,14 +16,18 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            
+            if (Suite.Capacidade>= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
+               
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                  throw new InvalidOperationException("A quantidade de hóspedes é maior que a capacidade da suíte.");
+                
             }
         }
 
@@ -37,24 +40,30 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
-        }
+            return Hospedes.Count;
+            
+         }
 
         public decimal CalcularValorDiaria()
         {
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10 )
             {
-                valor = 0;
+             decimal desconto = valor *0.10m;
+
+              valor -=desconto;
+
             }
 
             return valor;
+
         }
     }
 }
